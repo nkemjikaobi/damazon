3a1ca9037be106509aa471782eec226e1815a420
Obi NkemjIka Derick <obinkemjikaderick@Administrators-MacBook-Pro-6.local>
2021-03-25 12:35:52 +0100

    Prodcts Details Screen

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,7 +5,7 @@ import { Navbar , Nav , Container } from 'react-bootstrap';
 const Header = () => {
     return (
         <header>
-            <Navbar bg="dark" variant='dark' expand="lg" collapseonSelect>
+            <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
                 <Container>
                     <LinkContainer to='/'>
                         <Navbar.Brand >Damazon</Navbar.Brand>

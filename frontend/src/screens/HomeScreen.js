b3e96d825e001bd4b86e9dbcc4500e989a900541
Obi NkemjIka Derick <obinkemjikaderick@Administrators-MacBook-Pro-6.local>
2021-03-25 14:37:51 +0100

    Fetching products from React(UseEffect)

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,22 @@
-import React from 'react'
-import products from '../products'
+import React, { useState , useEffect } from 'react'
 import { Row , Col } from 'react-bootstrap'
 import Product from '../components/Product'
+import axios from 'axios'
 
 const HomeScreen = () => {
+
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => { //cannot make useffect async
+        const getProducts = async () => {
+            const { data } = await axios.get('/api/products'); //destructure data from res.
+
+            setProducts(data);
+        }
+
+        getProducts();
+    }, [])
+
     return (
         <>
          <h1>Latest Products</h1>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Col, Row, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import  Rating from '../components/Rating'
-import products from '../products'
+import axios from 'axios'
 
 
 const ProductScreen = ({ match }) => { 
 
-    const product = products.find(p => p._id === match.params.id)
+   const [product, setProduct] = useState({});
+
+   useEffect(() => { //cannot make useffect async
+    const getProduct = async () => {
+        const { data } = await axios.get(`/api/products/${match.params.id}`); //destructure data from res.
+
+        setProduct(data);
+    }
+
+    getProduct();
+}, [match.params.id])
     
     return (
         <>
